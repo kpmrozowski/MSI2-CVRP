6043a3cb35aab808f19b6075eea32f41c1ce6953
kmro <k.mrozowski@versabox.pl>
2021-03-03 10:57:14 +0100

    Added CMake

--- makefile
+++
@@ -1,2 +0,0 @@
-default:
-	g++ main.cpp -o out
---
+++ out/build/CMakeFiles/CVRP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVRP.dir/main.cpp.o"
+  "CVRP"
+  "CVRP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVRP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
