054a370cc6b4c7d960580e8746eced1a9fc00921
kornel <kpmrozowski@gmail.com>
2021-03-16 23:39:22 +0100

    added: Vechicle to src/CVRP

---
+++ include/MSI/CVRP/Vechicle.h
@@ -0,0 +1,16 @@
+#ifndef CVRP_VECHICLE_H
+#define CVRP_VECHICLE_H
+#include "../Util/IRandomGenerator.h"
+#include "../AntSystem/Tour.h"
+#include <vector>
+
+namespace msi::cvrp {
+
+class Vechicle {
+  public:
+  Vechicle();
+};
+
+} // namespace msi::cvrp
+
+#endif //CVRP_VECHICLE_H
---
+++ src/CVRP/Vechicle.cpp
@@ -0,0 +1,8 @@
+#include <MSI/CVRP/Vechicle.h>
+#include <fmt/core.h>
+
+namespace msi::cvrp {
+
+Vechicle::Vechicle() {}
+    
+} // namespace msi::cvrp 
