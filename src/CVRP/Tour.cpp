246b19ca2a9eb96b44ef938a72882a3a25bc6325
kpmrozowski <kpmrozowski@gmail.com>
2021-04-13 21:33:10 +0200

    evo: main::opengl.draw: fix needed!

--- src/CVRP/Tour.cpp
+++ src/CVRP/Tour.cpp
@@ -46,6 +46,7 @@ void Tour::run() noexcept {
 
          VertexId selected_vert = 0;
          auto feasible_vertex_count = std::count(feasible_vertices.begin(), feasible_vertices.end(), true);
+         m_unvisited_verts[0] = true;
 
          if (feasible_vertex_count > 1) {
             selected_vert = m_vehicles[vehicle_id].choose_next(m_graph, m_rand, feasible_vertices);
@@ -53,17 +54,29 @@ void Tour::run() noexcept {
                m_vehicles[vehicle_id].m_capacity_left -= m_graph.m_vertices[selected_vert].demand;
                m_unvisited_verts[selected_vert] = false;
             }
+         // } else if (feasible_vertex_count == 1) {
+         //    auto it_unvisited_vertex = std::find(m_unvisited_verts.begin(), m_unvisited_verts.end(), true);
+         //    selected_vert = it_unvisited_vertex  - m_unvisited_verts.begin();
+         //    if (selected_vert != m_vehicles[vehicle_id].m_current_vert) {
+         //       m_vehicles[vehicle_id].m_capacity_left -= m_graph.m_vertices[selected_vert].demand;
+         //       m_unvisited_verts[selected_vert] = false;
+         //    }
          } else {
             selected_vert = m_params.depot_id;
             m_ant_completed[vehicle_id] += 1;
             m_vehicles[vehicle_id].m_capacity_left = m_params.initial_capacity;
             m_unvisited_verts[0] = false;
          }
+         // fmt::print("cl{} ", m_vehicles[vehicle_id].m_capacity_left);
 
          m_vehicles[vehicle_id].m_visited_edges.emplace_back(m_vehicles[vehicle_id].m_current_vert, selected_vert);
          m_vehicles[vehicle_id].m_visited.insert(selected_vert);
          m_vehicles[vehicle_id].m_current_vert = selected_vert;
       }
+
+      // m_vehicles[vehicle_id].m_visited_edges.emplace_back(m_vehicles[vehicle_id].m_current_vert, m_params.depot_id);
+      // m_vehicles[vehicle_id].m_visited.insert(m_params.depot_id);
+      // m_vehicles[vehicle_id].m_current_vert = m_params.depot_id;
 
       this->update_pheromone();
    }
